f815b979783c29e2b8d74974b9dc806aa5a3fbfa
LTang <tangthree.212@gmail.com>
2020-01-18 16:54:07 -0800

    Update numbers_lecture_notes.py

---
+++ numbers_lecture_notes.py
@@ -0,0 +1,10 @@
+# Numbers - Intergers, floats
+# Interger - whole number, no decimal
+
+print(type(4))
+print(type(3.5))
+result = 4 + 4
+print(type(result))
+print(result)
+print(result + 4)
+print(abs(-10))
